62edc0ee07c5ac35f71eef35dd084c4edb0c92ca
梦宇@极客学院 <xtiqin@163.com>
2015-12-16 18:11:27 +0800

    完成ListView示例

--- app/src/main/java/com/plter/aila/demos/RecyclerViewDemoActivity.java
+++ app/src/main/java/com/plter/aila/demos/RecyclerViewDemoActivity.java
@@ -3,7 +3,6 @@ package com.plter.aila.demos;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.view.ViewGroup;
 
 /**
  * Created by plter on 12/2/15.
@@ -17,26 +16,5 @@ public class RecyclerViewDemoActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recyclerview_demo);
-
-        rv = (RecyclerView) findViewById(R.id.item_touch_helper_previous_elevation);
-        rv.setAdapter(new RecyclerView.Adapter() {
-            @Override
-            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-
-
-                return null;
-            }
-
-            @Override
-            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
-            }
-
-            @Override
-            public int getItemCount() {
-                return 0;
-            }
-        });
     }
 }
